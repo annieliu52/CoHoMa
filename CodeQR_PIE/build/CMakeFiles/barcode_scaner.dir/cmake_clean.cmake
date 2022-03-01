52133478def480d707f7b76aa56e69239458de0b
Nebnoma9 <94896393+Nebnoma9@users.noreply.github.com>
2022-03-02 00:06:00 +0100

    Add files via upload

---
+++ CodeQR_PIE/build/CMakeFiles/barcode_scaner.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/barcode_scaner.dir/qrcodeTest.cpp.o"
+  "barcode_scaner"
+  "barcode_scaner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/barcode_scaner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CodeQR_PIE/qrcodeTest.cpp
@@ -0,0 +1,114 @@
+#include <opencv2/opencv.hpp>
+#include <zbar.h>
+
+using namespace cv;
+using namespace std;
+using namespace zbar;
+
+typedef struct
+{
+  string type;
+  string data;
+  vector <Point> location;
+} decodedObject;
+
+// Find and decode barcodes and QR codes
+void decode(Mat &im, vector<decodedObject>&decodedObjects)
+{
+  
+  // Create zbar scanner
+  ImageScanner scanner;
+
+  // Configure scanner
+  scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
+  
+  // Convert image to grayscale
+  Mat imGray;
+  
+  cvtColor(im, imGray,COLOR_BGR2GRAY);
+
+  // Wrap image data in a zbar image
+  Image image(im.cols, im.rows, "Y800", (uchar *)imGray.data, im.cols * im.rows);
+  
+  // Scan the image for barcodes and QRCodes
+  int n = scanner.scan(image);
+  
+  // Print results
+  for(Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol)
+  {
+    decodedObject obj;
+    
+    obj.type = symbol->get_type_name();
+    obj.data = symbol->get_data();
+    
+    // Print type and data
+    cout << "Type : " << obj.type << endl;
+    cout << "Data : " << obj.data << endl << endl;
+    
+    // Obtain location
+    for(int i = 0; i< symbol->get_location_size(); i++)
+    {
+      obj.location.push_back(Point(symbol->get_location_x(i),symbol->get_location_y(i)));
+    }
+    
+    decodedObjects.push_back(obj);
+  }
+}
+
+// Display barcode and QR code location  
+void display(Mat &im, vector<decodedObject>&decodedObjects)
+{
+  // Loop over all decoded objects
+  for(int i = 0; i < decodedObjects.size(); i++)
+  {
+    vector<Point> points = decodedObjects[i].location;
+    vector<Point> hull;
+    
+    // If the points do not form a quad, find convex hull
+    if(points.size() > 4)
+      convexHull(points, hull);
+    else
+      hull = points;
+    
+    // Number of points in the convex hull
+    int n = hull.size();
+    
+    for(int j = 0; j < n; j++)
+    {
+      line(im, hull[j], hull[ (j+1) % n], Scalar(255,0,0), 3);
+    }
+    
+  }
+  
+  // Display results 
+  //resizeWindow("Results", WIDTH, HEIGHT);
+  namedWindow("Results",WINDOW_NORMAL);
+  imshow("Results", im);
+  waitKey(0);
+  
+}
+
+int main(int argc, char* argv[])
+{
+  
+  // Read image
+  //Mat im = imread("/home/alban/CodeQR_PIE/test4.jpg");
+  //Mat im = imread("/home/alban/CodeQR_PIE/Cohoma_qrcodes.png");
+  //Mat im0 = imread("/home/alban/CodeQR_PIE/test3.jpg");
+  //Mat im = imread("/home/alban/CodeQR_PIE/zbar-test.jpg");
+  //Mat im = imread("/home/alban/CodeQR_PIE/test_img.jpg");
+  Mat im = imread("/home/alban/CodeQR_PIE/scree.jpg");
+   
+  // Variable for decoded objects 
+  vector<decodedObject> decodedObjects, decodedObjects0;
+  
+  // Find and decode barcodes and QR codes
+  decode(im, decodedObjects);
+  //decode(im0, decodedObjects0);
+
+  // Display location 
+  display(im, decodedObjects);
+  //display(im0, decodedObjects0);
+  
+  return EXIT_SUCCESS;
+}
